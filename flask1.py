85d990e89baab993a0ce05b2b981ad37536c922d
sudesh <you@example.com>
2018-03-17 16:23:31 +0530

    first commit

---
+++ flask1.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return"homepage"
+@app.route("/sudesh")
+
+def homen():
+    return" <html> <h1>Testing the flask </h1></html>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
